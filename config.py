c4061ddb3416124c768566989e589da7432232f8
CleoMugs <cleophas.mugeni@gmail.com>
2020-05-28 20:58:29 +0300

    first commit

--- config.py
+++ config.py
@@ -43,9 +43,7 @@ class TestingConfig(Config):
 	"""
 
 	#TESTING = True
-	#SECRET_KEY = '8f1eb85917a0cee05fecac007d23664f'
-	#SQLALCHEMY_DATABASE_URI = 'sqlite:///members_test.db'
-
+	
 
 
 app_config = {
@@ -55,17 +53,3 @@ app_config = {
 }
 
 
-
-'''
-	SECRET_KEY = '8f1eb85917a0cee05fecac007d23664f'
-	SQLALCHEMY_DATABASE_URI = 'sqlite:///members.db'
-	MAIL_SERVER = 'smtp.gmail.com'
-	MAIL_PORT = 587
-	MAIL_USE_TLS = True
-
-	MAIL_USERNAME = os.environ.get('EMAIL_USER')
-	MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-
-	#.['MAIL_USERNAME'] = 'cleophas.mugeni@gmail.com'
-	#'MAIL_PASSWORD'] = 'Kakosh@123'
-'''
