de94a0f9b61f04f1efb124dccfdf2fb6644aa7d0
CleoMugs <cleophas.mugeni@gmail.com>
2020-06-02 18:11:48 +0300

    added config files

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -19,9 +19,21 @@ login_manager.login_message_category = 'info'
 mail = Mail()
 
 def create_app(config_name): 
-	app = Flask(__name__, instance_relative_config=True)
-	app.config.from_object(app_config[config_name])
-	app.config.from_pyfile('config.py')
+
+	if os.environ.get('FLASK_ENV') == "production":
+		app = Flask(__name__)
+		app.config.update(SECRET_KEY=os.environ.get('SECRET_KEY'), SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI'))
+			
+
+	else:
+		app = Flask(__name__, instance_relative_config=True)
+		app.config.from_object(app_config[config_name])
+		app.config.from_pyfile('config.py')
+
+
+	#app = Flask(__name__, instance_relative_config=True)
+	#app.config.from_object(app_config[config_name])
+	#app.config.from_pyfile('config.py')
 
 	db.init_app(app)
 	bcrypt.init_app(app)
@@ -47,3 +59,20 @@ def create_app(config_name):
 	'''
 
 	return app
+
+
+'''
+mport os
+import sys
+
+path = '/home/Cleo/userLogin3'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['FLASK_ENV'] = 'production'
+os.environ['SECRET_KEY'] = '8f1eb85917a0cee05fecac007d23664f'
+os.environ['SQLALCHEMY_DATABASE_URI'] = 'mysql://Cleo:kakosh123@Cleo.mysql.pythonanywhere-services.com/Cleo$members'
+SQLALCHEMY_DATABASE_URI'='mysql://Cleo:kakosh123@Cleo.mysql.pythonanywhere-services.com/Cleo$members'
+
+from run import app as application
+'''
